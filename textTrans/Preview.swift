8ae511bbca1bda20ee5b807a441c2b54f4f3d85a
Mubi <mubashirasaad@hotmail.com>
2018-05-07 15:48:14 +0500

    first commit

---
+++ textTrans/Preview.swift
@@ -0,0 +1,9 @@
+//
+//  Preview.swift
+//  textTrans
+//
+//  Created by Mubashir Asaad on 03/05/2018.
+//  Copyright © 2018 Mubashir Asaad. All rights reserved.
+//
+
+import Foundation
---
+++ textTrans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textTrans
+//
+//  Created by Mubashir Asaad on 03/05/2018.
+//  Copyright © 2018 Mubashir Asaad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
